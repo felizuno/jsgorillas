ad976068ae573e5c87b1e1f12c06ea856ff8b92a
Kevin Ewing <notmyuwmail@gmail.com>
2013-07-20 11:32:57 -0700

    Animator Start

--- js/viewmanager/animator.js
+++ js/viewmanager/animator.js
@@ -2,7 +2,36 @@
   
   App.viewManager.canvasManager.animator = {
     children: [],
-    handlers: {},
+    handlers: {
+      draw: 'queueFrame'
+    },
+
+    animFrameQueue: [],
+    runningFameQueue: false,
+
+    queueFrame: function(pM) {
+      this.animFrameQueue.push(pM.payload);
+      if (!this.runningFameQueue) {
+        this._runFrameQueue();
+      }
+    },
+
+    _runFrameQueue: function() {
+      var self = this;
+
+      this.runningFameQueue = true;
+
+      var runQueue = function() {
+        _.each(self.animFrameQueue, function(frameFunc) {
+console.log('frame', frameFunc)
+          frameFunc();
+        })
+      };
+
+      requestAnimationFrame(runQueue);
+
+      this.runningFameQueue = false;
+    }
   };
 
 })();
